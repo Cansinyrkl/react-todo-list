a8dd29d0a43c7766592139857336c031ba329ae8
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-09-11 11:19:04 +0300

    login confirmation

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,33 +1,61 @@
 import React, { useState } from "react";
 import "./loginform.css";
+import admin from "./userspages/admin";
 
 const LoginForm = () => {
   const [username, setUserName] = useState([
-    { id: 1, name: "admin", password: 123, admin: true },
-    { id: 2, name: "admin1", password: 123, admin: true },
-    { id: 3, name: "user", password: 123, admin: false },
-    { id: 4, name: "user2", password: 123, admin: false },
+    { id: 1, name: "admin", password: "123", admin: true },
+    { id: 2, name: "admin1", password: "123", admin: true },
+    { id: 3, name: "user", password: "123", admin: false },
+    { id: 4, name: "user2", password: "123", admin: false },
   ]);
 
-  const submit = (e) => {
-    e.preventDefault();
-    console.log(e);
+  const [userDatasHolder, setUserDatasHolder] = useState();
+  const [passawordDatasHolder, setPasswordDatasHolder] = useState();
+
+  const userDataHolder = (e) => {
+    const userNameValue = e.target.value;
+    setUserDatasHolder(userNameValue);
+  };
+  const passawordDataHolder = (e) => {
+    const passawordValue = e.target.value;
+    setPasswordDatasHolder(passawordValue);
+  };
+
+  const btnClick = () => {
+    const userNa = username.map((item) => item.name);
+    const userPa = username.map((item) => item.password);
+
+    const Us = userNa.includes(userDatasHolder);
+    const Pa = userPa.includes(passawordDatasHolder);
+
+    if (Us && Pa) {
+      console.log("başarılı");
+    } else {
+      console.log("başarısız");
+    }
   };
 
   return (
     <div className="cover">
       <h1>Login</h1>
-      <form>
-        <input type="text" name="userName" placeholder="username" />
-        <input type="password" name="password" placeholder="password" />
-        <button type="submit" onClick={submit} className="login-btn">
-          Login
-        </button>
+      <form className="form-container">
+        <input
+          type="text"
+          name="userName"
+          placeholder="username"
+          onChange={userDataHolder}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={passawordDataHolder}
+        />
       </form>
-      <div>
-        <h3>Login Failed</h3>
-        <p>Username or password incorrect</p>
-      </div>
+      <button type="submit" onClick={btnClick} className="login-btn">
+        Login
+      </button>
     </div>
   );
 };

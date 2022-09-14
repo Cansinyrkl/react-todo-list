d91699ec26cd195ba36bed82b4a35817ca922a66
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-09-14 12:35:38 +0300

    todo list trying

--- src/components/userspages/admin.js
+++ src/components/userspages/admin.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "./admin.css";
+
+const list = ["Elma", "Armut", "Kel Mahmut", "Kel Hasan"];
+list.push("Hasan", "Hüsso");
+
+document.getElementById("deneme").innerHTML = list;
 
 const Admin = () => {
-  return <div>Buradaysan Başardın adminsin</div>;
+  return (
+    <>
+      <div className="admin-page">
+        <div className="admin-cover">
+          <h1>Todo List</h1>
+          <ul className="todo-list">
+            <li id="deneme"></li>
+          </ul>
+
+          <input type="text" id="todoInput" placeholder="Todo List" />
+
+          <button type="submit" className="admin-login-btn">
+            Login
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Admin;

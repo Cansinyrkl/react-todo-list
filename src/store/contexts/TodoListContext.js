76ecd5b67a37c3abea1b765c8345a87b28527d20
Ahmet Can Yürekli <ayurekl97@gmail.com>
2022-09-20 12:08:50 +0300

    feat: todo context created.

---
+++ src/store/contexts/TodoListContext.js
@@ -0,0 +1,19 @@
+import React, { createContext } from "react";
+
+export const TodoListContext = createContext();
+
+const TodoListContextProvider = ({ children }) => {
+  const [todos, setTodos] = useState([
+    { id: 1, todo: "yumurta" },
+    { id: 2, todo: "yumurta" },
+    { id: 3, todo: "yumurta" },
+  ]);
+};
+
+return (
+  <TodoListContext.Provider value={{ todos, setTodos }}>
+    {children}
+  </TodoListContext.Provider>
+);
+
+export default TodoListContextProvider;
